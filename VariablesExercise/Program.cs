c253ee2399637469538b668ed76e2732cb27a42d
Demitri Cameron <oblivion9794@gmail.com>
2022-11-21 11:42:21 -0500

    initial commit

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,29 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            string name = "Demitri";
+
+            int a = 117;
+
+            char Spotify = 'S';
+
+            bool x = true;
+
+            double longpi = 3.14159265358979D;
+
+            decimal lifeofpi = 3.14m;
+
+
+            Console.WriteLine($"My name is {name}, I feel {a} years old.");
+            Console.WriteLine($"I listen to music on spotify, but it should be a capital {Spotify}.");
+            Console.WriteLine($"This is {x}.");
+            Console.WriteLine($"A lot of digits for Pi are {longpi}.");
+            Console.WriteLine($"It is usually abbreviated to {lifeofpi}."
+   
+                );
+
+
         }
     }
 }
